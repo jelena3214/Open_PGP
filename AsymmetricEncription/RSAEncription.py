e08c95419909be614542073e5480b3ca5f8241e7
jelena3214 <jelena.cvetic3214@gmail.com>
2024-05-24 16:46:37 +0200

    Initial commit

---
+++ AsymmetricEncription/RSAEncription.py
@@ -0,0 +1,18 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from Exceptions.PGP_Exception import PGP_Exception
+
+class RSAEncription:
+
+    @classmethod
+    def generate_rsa_key_set(cls, key_size):
+        if key_size != 1024 and key_size != 2048:
+            raise PGP_Exception("Key size for RSA is not valid!")
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=key_size,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
+
+        return public_key, private_key
---
+++ Exceptions/PGP_Exception.py
@@ -0,0 +1,5 @@
+class PGP_Exception(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
